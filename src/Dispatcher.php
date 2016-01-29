ebd819896f8b263378567fe40329789c88ca5fd9
Ilija Studen <ilija.studen@activecollab.com>
2016-01-29 16:59:15 +0100

    Initial commit

---
+++ src/Dispatcher.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Promises.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace ActiveCollab\Promises;
+
+/**
+ * @package ActiveCollab\Promises
+ */
+class Dispatcher implements DispatcherInterface
+{
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Promises.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Bootstrap test case.
+ */
+date_default_timezone_set('GMT');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+require __DIR__ . '/src/TestCase.php';
